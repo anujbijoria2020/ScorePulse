6a3df217bf14fb5853535e14d4c422af9574f450
Anuj Patel <bhaianujkurmi2@gmail.com>
2026-02-20 19:44:59 +0530

    implemented fixes

--- src/ws.js
+++ src/ws.js
@@ -28,13 +28,34 @@ export function attachWebSocketServer(server){
     });
 
     wss.on("connection",(socket)=>{
+         socket.isAlive = true;
+         socket.on("pong",()=>{
+            socket.isAlive = true;
+            });
+
         console.log("New WebSocket connection established");
+
         sendJSON(socket,{type:"welcome",message:"Welcome to the Sports Match API WebSocket!"});
 
         socket.on("error",(error)=>{
             console.log("WebSocket error: ",error);
         });
     });
+
+    const Interval = setInterval(()=>{
+        wss.clients.forEach((socket)=>{
+            if(socket.isAlive===false){
+                return socket.terminate();
+            }
+            socket.isAlive = false;
+            socket.ping();
+        })
+    },30000)
+
+    wss.on("close",()=>{
+        clearInterval(Interval);
+    });
+
 
     function broadcastMatchCreated(match){
         broadcast(wss,{type:"matchCreated",data:match});
